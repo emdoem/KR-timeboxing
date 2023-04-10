e285862a8fb2ae933213bd9d891e6919c728e0d2
emdoem <125324546+emdoem@users.noreply.github.com>
2023-04-10 12:44:47 +0200

    extract App component

---
+++ src/components/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import { TimeboxList, EditableTimebox } from "./remaining-components"
+function App() {
+    return (
+        <div className="App">
+            <TimeboxList />
+            <EditableTimebox />
+        </div>
+    )
+}
+
+export default App;
