ab6aae097a6f1e372c89cc2c4223cc4ef77c44a6
emdoem <125324546+emdoem@users.noreply.github.com>
2023-04-10 15:38:42 +0200

    export styles to main.css

--- src/index.js
+++ src/index.js
@@ -2,9 +2,6 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 
 import App from "./components/App";
-
-
-
-
+import "./styles/main.css"
 
 ReactDOM.render(<App />, document.getElementById("root"));
