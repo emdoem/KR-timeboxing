3f4b500308a190db95f18b3020af0d422ab40eea
emdoem <125324546+emdoem@users.noreply.github.com>
2023-04-11 17:23:06 +0200

    use BEM to style ProgressBar

--- src/components/ProgressBar.js
+++ src/components/ProgressBar.js
@@ -4,8 +4,8 @@ function ProgressBar({ className = "", percent = 33, timeLeft = 15*60, totalTime
     // passing state variables into timeLeft/totalTime to make ProgressBar follow the timer
     percent = timeLeft/totalTime*100
     return (
-        <div className={"ProgressBar " + className}>
-            <div style={{width: `${percent}%`}}></div>
+        <div className={"progress progress--big progress--color-purp" + className}>
+            <div className="progress__bar" style={{width: `${percent}%`}}></div>
         </div>
     );
 }
