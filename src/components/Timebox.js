6af20c2c316f5714bba822c8098c807b791be7df
emdoem <125324546+emdoem@users.noreply.github.com>
2023-05-15 17:00:05 +0200

    add default props and prop types to Timebox comp

--- src/components/Timebox.js
+++ src/components/Timebox.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 
 class Timebox extends React.Component {
     constructor(props) {
@@ -35,6 +36,17 @@ class Timebox extends React.Component {
             </div>
         )
     }
+}
+Timebox.defaultProps = {
+    onDelete: console.log('Delete has been issued.'),
+    onEdit: console.log('Edit has been issued.')
+}
+Timebox.propTypes = {
+    key: PropTypes.string.isRequired, // ={timebox.id} 
+    title: PropTypes.string.isRequired, // ={timebox.title} 
+    totalTimeInMinutes: PropTypes.number.isRequired, // ={timebox.totalTimeInMinutes} 
+    onDelete: PropTypes.func.isRequired, // ={() => this.removeTimebox(index)}
+    onEdit: PropTypes.func.isRequired
 }
 
 export default Timebox;
