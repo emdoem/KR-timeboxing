2837424dbb067373f178cea2ad5c7eb1cbde4a05
emdoem <125324546+emdoem@users.noreply.github.com>
2023-05-15 16:15:51 +0200

    use defaultProps and PropTypes in Clock component

--- src/components/Clock.js
+++ src/components/Clock.js
@@ -1,6 +1,7 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 
-function Clock({ className = "", minutes = 7, seconds = 0 }) {
+function Clock({ className, minutes, seconds }) {
     // here making sure the numbers display as double digits
     let minutesDisplay = minutes.toString();
     if (minutesDisplay.length < 2) {
@@ -12,6 +13,15 @@ function Clock({ className = "", minutes = 7, seconds = 0 }) {
     };
     
     return <h2 className={"Clock " + className}>Pozostało {minutesDisplay}:{secondsDisplay}</h2>
+}
+Clock.defaultProps = {
+    className: ""
+}
+const NumberOrStringType = PropTypes.oneOfType([PropTypes.number, PropTypes.string]);
+Clock.propTypes = {
+    className: PropTypes.string.isRequired, 
+    minutes: NumberOrStringType.isRequired, 
+    seconds: NumberOrStringType.isRequired
 }
 
 export default Clock;
