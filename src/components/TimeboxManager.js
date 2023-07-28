fa06b56e4143a29b9faaa53340a9c844077f034a
emdoem <125324546+emdoem@users.noreply.github.com>
2023-07-28 20:16:25 +0200

    useDispatch and useSelector for TimeboxesManager

--- src/components/TimeboxManager.js
+++ src/components/TimeboxManager.js
@@ -8,10 +8,9 @@ import AuthenticationContext from '../contexts/AuthenticationContexts';
 import { AllTimeboxesList } from './TimeboxesList';
 import ReadOnlyTimebox from './ReadOnlyTimebox';
 import { areTimeboxesLoading, getTimeboxesLoadingError } from './timeboxesReducer';
-import { setTimeboxes, setError, disableLoadingIndicator, addTimebox, stopEditingTimebox, replaceTimebox, removeTimebox, startEditingTimebox } from './TimeboxesManagerActions';
+import { fetchAllTimeboxes, setTimeboxes, setError, disableLoadingIndicator, addTimebox, stopEditingTimebox, replaceTimebox, removeTimebox, startEditingTimebox } from './TimeboxesManagerActions';
 
-import { useForceUpdate } from './reduxStore';
-import { useStore } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
 import { EditableTimebox } from './EditableTimebox.1';
 
 export const Timebox = React.lazy(() => import('./Timebox'));
@@ -21,19 +20,16 @@ const TimeboxesAPI = createTimeboxesAPI("http://localhost:5000/timeboxes/");
 
 function TimeboxManager() {
 
-    // const [state, dispatch] = useReducer(timeboxesReducer, undefined, timeboxesReducer);
-    const store = useStore();
-    const forceUpdate = useForceUpdate();
-    const state = store.getState().timeboxesManager;
-    const dispatch = store.dispatch;
-    useEffect(() => store.subscribe(forceUpdate), []);
-
+    const dispatch = useDispatch();
+    const timeboxesLoading = useSelector(areTimeboxesLoading);
+    const timeboxesLoadingError = useSelector(getTimeboxesLoadingError);
     const { accessToken } = useContext(AuthenticationContext);
 
     useEffect(() => {
         TimeboxesAPI.getAllTimeboxes(accessToken).then(
             (timeboxes) => {
                 dispatch(setTimeboxes(timeboxes))
+                // console.table(timeboxes)
             }
         ).catch(
             (error) => dispatch(setError(error))
@@ -81,11 +77,12 @@ function TimeboxManager() {
         />
     }
 
+
     return (
         <>
             <TimeboxCreator onCreate={handleCreate} />
-            {areTimeboxesLoading(state) ? "Timeboxy się ładują..." : null}
-            {getTimeboxesLoadingError(state) ? "Coś się wykrzaczyło w liście :(" : null}
+            {timeboxesLoading ? "Timeboxy się ładują..." : null}
+            {timeboxesLoadingError ? "Coś się wykrzaczyło w liście :(" : null}
             <Error message="Coś się wykrzaczyło w liście :(">
                 <AllTimeboxesList
                     renderTimebox={renderTimebox}
