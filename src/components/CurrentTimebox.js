3cd3643737cc260035b55a17b73769aab7d39b30
emdoem <125324546+emdoem@users.noreply.github.com>
2023-07-28 13:10:38 +0200

    useDispatch & useSelector for CurrentTimebox

--- src/components/CurrentTimebox.js
+++ src/components/CurrentTimebox.js
@@ -2,23 +2,22 @@ import React, { useReducer, useRef, useEffect } from 'react';
 
 import Clock from "./Clock";
 import ProgressBar from "./ProgressBar";
-import { getMinsAndSecsFromSecs } from "../lib/time";
-import { currentTimeboxReducer, initialState } from './currentTimeboxReducer';
+import { getPauseStatus, getRunningStatus, getPausesCount, getElapsedTimeInSeconds } from './currentTimeboxReducer';
 
-import { useForceUpdate } from './reduxStore';
-import { useStore } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
 
 
 const runTimer = () => ({ type: "TIMER_RUNNING" });
 
 function CurrentTimebox({ title, totalTimeInMinutes }) {
 
-    // const [state, dispatch] = useReducer(currentTimeboxReducer, initialState, currentTimeboxReducer);
-    const store = useStore();
-    const forceUpdate = useForceUpdate();
-    const state = store.getState().currentTimebox;
-    const dispatch = store.dispatch;
-    useEffect(() => store.subscribe(forceUpdate), []);
+    const dispatch = useDispatch();
+    const isPaused = useSelector(state => getPauseStatus(state.currentTimebox));
+    const isRunning = useSelector(state => getRunningStatus(state.currentTimebox));
+    const pausesCount = useSelector(state => getPausesCount(state.currentTimebox));
+    //no idea why this one below works without reffering to the currentTimebox part of the state...
+    const elapsedTimeInSeconds = useSelector(getElapsedTimeInSeconds);
+    
     let intervalIdRef = useRef(null);
 
     function handleStart() {
@@ -43,14 +42,14 @@ function CurrentTimebox({ title, totalTimeInMinutes }) {
     }
     function togglePause() {
         dispatch({ type: "PAUSE_TOGGLE" })
-        if (state.isPaused) {
+        if (isPaused) {
             startTimer();
         } else {
             stopTimer();
         }
     }
     // console.table(state);
-    const { isPaused, isRunning, pausesCount, elapsedTimeInSeconds } = state;
+
     const totalTimeInSeconds = totalTimeInMinutes * 60;
     const timeLeftInSeconds = totalTimeInSeconds - elapsedTimeInSeconds;
 
